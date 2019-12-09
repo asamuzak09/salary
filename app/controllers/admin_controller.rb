39fd4b54f8239f14ef5cb7419efe1cab9bfe37e4
asamuzak09 <ekazumasa.09300201@gmail.com>
2019-12-09 17:03:40 +0900

    微修正２

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -16,37 +16,46 @@ class AdminController < ApplicationController
       closing_date: @date,
       starting_date: @date - 1.month + 1.day
     )
-    raise
+    
     (@timecard.starting_date..@timecard.closing_date).each do |date|
      if date.wday == 1
-      @start_at = Time.parse("#{params[:'mon_start(4i)']}:#{params[:'mon_start(5i)']}")
-      @rest = params[:mon_rest(4i)].to_i * 60 + params[:mon_rest(4i)].to_i
-      @end_at = Time.parse("#{params[:'mon_end(4i)']}:#{params[:'mon_end(5i)']}")
+        @start_at = Time.parse("#{params[:'mon_start(4i)']}:#{params[:'mon_start(5i)']}")
+        @rest = params[:"mon_rest(4i)"].to_i * 60 + params[:"mon_rest(5i)"].to_i
+        @end_at = Time.parse("#{params[:'mon_end(4i)']}:#{params[:'mon_end(5i)']}")
      elsif date.wday == 2
-      
+        @start_at = Time.parse("#{params[:'tue_start(4i)']}:#{params[:'tue_start(5i)']}")
+        @rest = params[:"tue_rest(4i)"].to_i * 60 + params[:"tue_rest(5i)"].to_i
+        @end_at = Time.parse("#{params[:'tue_end(4i)']}:#{params[:'tue_end(5i)']}")
      elsif date.wday == 3
-
+        @start_at = Time.parse("#{params[:'wed_start(4i)']}:#{params[:'wed_start(5i)']}")
+        @rest = params[:"wed_rest(4i)"].to_i * 60 + params[:"wed_rest(5i)"].to_i
+        @end_at = Time.parse("#{params[:'wed_end(4i)']}:#{params[:'wed_end(5i)']}")
      elsif date.wday == 4
-      
+        @start_at = Time.parse("#{params[:'thu_start(4i)']}:#{params[:'thu_start(5i)']}")
+        @rest = params[:"thu_rest(4i)"].to_i * 60 + params[:"thu_rest(5i)"].to_i
+        @end_at = Time.parse("#{params[:'thu_end(4i)']}:#{params[:'thu_end(5i)']}")
      elsif date.wday == 5
-      
+        @start_at = Time.parse("#{params[:'fri_start(4i)']}:#{params[:'fri_start(5i)']}")
+        @rest = params[:"fri_rest(4i)"].to_i * 60 + params[:"fri_rest(5i)"].to_i
+        @end_at = Time.parse("#{params[:'fri_end(4i)']}:#{params[:'fri_end(5i)']}")
      elsif date.wday == 6
-
+        @start_at = Time.parse("#{params[:'sat_start(4i)']}:#{params[:'sat_start(5i)']}")
+        @rest = params[:"sat_rest(4i)"].to_i * 60 + params[:"sat_rest(5i)"].to_i
+        @end_at = Time.parse("#{params[:'sat_end(4i)']}:#{params[:'sat_end(5i)']}")
      else
-      
+        @start_at = Time.parse("#{params[:'sun_start(4i)']}:#{params[:'sun_start(5i)']}")
+        @rest = params[:"sun_rest(4i)"].to_i * 60 + params[:"sun_rest(5i)"].to_i
+        @end_at = Time.parse("#{params[:'sun_end(4i)']}:#{params[:'sun_end(5i)']}")
      end 
-
-    #  @shift = Shift.create(
-    #    time_card_id: @timecard.id,
-    #    kind:kind(),
-    #    date:date,
-    #    start_at: @start_at
-    #    rest_minutes: @rest
-    #    end_at: @end_at
-    #    preparation:
-
-
-    #  )
+      @shift = Shift.create(
+       time_card_id: @timecard.id,
+       kind:kind(@start_at,@end_at),
+       date:date,
+       start_at: @start_at,
+       rest_minutes: @rest,
+       end_at: @end_at,
+       preparation: Time.parse("#{params[:'preparation(4i)']}:#{params[:'preparation(5i)']}")
+      )
     end
     
     redirect_to("/admin/edit/#{@timecard.id}")
@@ -54,6 +63,7 @@ class AdminController < ApplicationController
   
   def edit
     @timecard = TimeCard.find_by(id: params[:id])
+  
   end 
   
   def kind(start,ending)
@@ -62,6 +72,10 @@ class AdminController < ApplicationController
     else
       :working_day  
     end  
-  end  
+  end
+  
+  # # def worktime(start,rest,ending)
+  #   "(ending - start ).floor/3600 - rest/60}".rjust(2,"0") + ":" + "#{((ending - start).floor%3600)/60 - rest%60}".rjust(2,"0")
+  # end  
   
 end
